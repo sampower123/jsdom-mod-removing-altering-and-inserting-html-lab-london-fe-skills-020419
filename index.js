2aab16c1f2893c5764a15d9307d7b7cd99c8b849
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
 
-
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+
