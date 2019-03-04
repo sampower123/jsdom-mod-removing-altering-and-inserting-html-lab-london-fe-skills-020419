54b6134129d318a107a6f8ff7f84b79e2e6b0bcd
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:20:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ main.remove()
 const newHeader = null;
 
 document.body.appendChild(element)
-\
+
+var ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
