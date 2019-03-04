753b7ad0ccbbb3d289bf4247c6f1a7b6f4fd1944
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 
-var h1 = document.createElement('h1')
-
-h1.class('newHeader')
+var h1 = document.createElement('h1');
+h1.id = 'newHeader';
