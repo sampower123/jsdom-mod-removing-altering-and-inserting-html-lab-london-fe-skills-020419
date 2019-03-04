96c62b0e1592332ee342c738d57bc0f8a407461f
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:21:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,4 @@ main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 
-document.body.appendChild(element)
-
-var ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
+var h1 = document.createElement('h1')
