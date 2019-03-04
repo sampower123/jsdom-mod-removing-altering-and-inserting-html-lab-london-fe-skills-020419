d97e08bcc71a7e341d8cbf85cae2db0ad2a52b82
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ var h1 = document.createElement('h1')
 document.body.appendChild(h1)
 
 h1.id = 'victory'
+
+var node = document.createElement("LI");                 // Create a <li> node
+var textnode = document.createTextNode("Water");         // Create a text node
+node.appendChild(textnode);                              // Append the text to <li>
+document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
