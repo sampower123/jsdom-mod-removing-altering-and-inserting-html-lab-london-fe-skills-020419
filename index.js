62c4ba17cca53b6e6e71139123ae747454b1450d
Sam Power <sam.g.power@gmail.com>
2019-03-04 19:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ main.remove()
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1');
 
-var h1 = document.createElement('h1')
-
 document.body.appendChild(h1)
 
 h1.id = 'victory'
